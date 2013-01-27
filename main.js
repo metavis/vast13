2521fb41b28cb1541e53a9831f6091b22b66efd6
Ziyuan Lin <ziyuang@outlook.com>
2013-01-27 15:10:54 +0200

    mini plot texts added explanation panel added

--- main.js
+++ main.js
@@ -8,13 +8,14 @@
 
 var SVGSize = 700;
 var MiniplotSize = 100;
-var CircleRadius = 2;
-var Margin=10;
+var CircleRadius = 3;
+var Margin = 10;
 var ScaleWithPadding = d3.scale.linear()
     .domain([0, 1])
     .range([Margin, SVGSize - Margin]);
 
 var PCAColorMap = CreatePCAColorMap(40);
+var PCAMiniplotText=CreatePCAMiniplotText(40);
 
 var circlePCAPrefix = "pca_circ_";
 var miniplotPCAPrefix = "pca_mini_";
@@ -37,7 +38,7 @@ function PlotJSON(divId, fileName, viewFolder) {
         .attr("height", SVGSize)
         .attr("fill", "none")
         .attr("stroke-width", 2)
-        .attr("stroke","black");
+        .attr("stroke", "black");
 
     d3.json(fileName, function (json) {
 
@@ -47,7 +48,7 @@ function PlotJSON(divId, fileName, viewFolder) {
             .append("circle")
             .attr("id", function (d, i) {
                 var idx = sprintf("%03d", i + 1);
-                return(circlePCAPrefix+idx);
+                return(circlePCAPrefix + idx);
             })
             .attr("cx", function (d) {
                 return(ScaleWithPadding(d[0]));
@@ -59,42 +60,50 @@ function PlotJSON(divId, fileName, viewFolder) {
             .attr("fill", function (d, i) {
                 return(PCAColorMap[i]);
             })
-            .on("mouseover", function(d, i){
-                var x=d3.mouse(this)[0];
-                var y=d3.mouse(this)[1];
-                var miniplot_id=sprintf("#%s%03d",miniplotPCAPrefix,i);
+            .on("mouseover", function (d, i) {
+                var x = d3.mouse(this)[0];
+                var y = d3.mouse(this)[1];
+                var miniplot_id = sprintf("#%s%03d", miniplotPCAPrefix, i);
                 d3.select(miniplot_id)
-                    .style("top",y+"px")
-                    .style("left",x+"px")
-                    .style("display","block");
+                    .style("top", y + "px")
+                    .style("left", x + "px")
+                    .style("display", "block");
 
             })
-            .on("mouseout", function(d,i){
-                var miniplot_id=sprintf("#%s%03d",miniplotPCAPrefix,i);
+            .on("mouseout", function (d, i) {
+                var miniplot_id = sprintf("#%s%03d", miniplotPCAPrefix, i);
                 d3.select(miniplot_id)
-                    .style("display","none");
+                    .style("display", "none");
             });
 
 
-        plot.selectAll("div")
+        var miniplots = plot.selectAll("div")
             .data(json)
             .enter()
-            .append("div")
-            .attr("id", function (d, i) {
+            .append("div");
+
+        miniplots.attr("id", function (d, i) {
                 var idx = sprintf("%03d", i + 1);
-                return(miniplotPCAPrefix+ idx);
+                return(miniplotPCAPrefix + idx);
             })
             .style("width", MiniplotSize + "px")
-            .style("height", MiniplotSize + "px")
-            .style("position","absolute")
-            .style("display", "none")
-            .append("img")
+            .style("height", (MiniplotSize+10) + "px")
+            .style("position", "absolute")
+            .style("display", "none");
+
+        miniplots.append("img")
             .attr("src", function (d, i) {
                 var idx = sprintf("%03d", i + 1);
                 return (viewFolder + "/" + idx + ".png");
             })
             .attr("width", MiniplotSize)
             .attr("height", MiniplotSize);
+
+        miniplots.append("p")
+            .append("small")
+            .text(function(d,i){
+                return(PCAMiniplotText[i]);
+            });
     });
 }
 
@@ -110,4 +119,18 @@ function CreatePCAColorMap(ndim) {
         }
     }
     return(colorMap);
+}
+
+function CreatePCAMiniplotText(ndim)
+{
+    var textArray = new Array(ndim * (ndim - 1) / 2);
+    var idx = 0;
+    for (var i = 1; i < ndim; i++) {
+        for (var j = i + 1; j <= ndim; j++) {
+            textArray[idx] = sprintf("$i=%d,j=%d$",i,j);
+            idx++;
+        }
+    }
+    return(textArray);
+
 }
